32cfc97d74dfb89bb5d9f9b5f0f96304ffc4c0ca
drrylpns <darrylpaniss@gmail.com>
2023-09-15 16:10:11 +0800

    firstcommit

---
+++ src/components/CloseModal.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import { Button } from './ui/Button'
+import { X } from 'lucide-react'
+
+
+const CloseModal = () => {
+
+    const router = useRouter()
+  return <Button 
+            variant='subtle' 
+            aria-label='close modal' 
+            className='h-6 w-6 p-0 rounded-md'
+            onClick={() => router.back()}>
+    <X className='h-4 w-4'/>
+  </Button>
+}
+
+export default CloseModal
